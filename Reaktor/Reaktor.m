a0900ce6ef8b186a604049e0c9b4153777d53991
Carl F <carl.fermergard@gmail.com>
2022-03-29 14:05:59 +0200

    struktur

--- Reaktor/Reaktor.m
+++ Reaktor/Reaktor.m
@@ -1,8 +1,13 @@
-% Reaktor
+% Reaktor KAA146 Grundläggande kemiteknik, Projektarbete grupp 4
+
 clc, clear
 
-test
-%% Cp beräkning
+% isobutan = A
+% isobuten = B
+% vätgas   = C
+% vatten   = D
+
+% Cp beräkning
 % H2O
 clc
 clear
@@ -18,3 +23,19 @@ B_H2 = -11.363417;
 C_H2 = 11.432816;
 D_H2 = 2.772874;
 Cp_H2 = (A_H2 + (B_H2.*(T.*10^-3)) + (C_H2.*((T.*10^-3).^2)) + (D_H2.*((T.*10^-3).^3)))
+
+%
+%Modell för reaktorn
+
+% isobutan = A
+% isobuten = B
+% vätgas   = C
+% vatten   = D
+
+clc, clear
+
+% Data
+R = 8.31447;
+Cp_D = 100;
+l = [0 10];     % Längd på reaktor1
+

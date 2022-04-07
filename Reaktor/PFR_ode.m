53eaaeb3d2d459dadcfc67ba459a2dcf7ad956a5
ronge11 <johan.ronge010327@gmail.com>
2022-04-07 14:49:50 +0200

    La in dH0

--- Reaktor/PFR_ode.m
+++ Reaktor/PFR_ode.m
@@ -1,4 +1,4 @@
-function dUdV = PFR_ode(U, Cp)
+function dUdV = PFR_ode(V, U, Cp, dH0, A, Ea)
 
 % A = isobutAn (C4H10)
 % B = isobutEn (C4H8)
@@ -6,32 +6,39 @@ function dUdV = PFR_ode(U, Cp)
 % D = vatten   (H2O)
 
 % Data
-R  = 8.31447; % gaskon.
-P  = 1;       % bar 
-k  = 0.0596;  % mol/kg cat.*s*bar vid 550 C
-K1 = 22.9;    % bar^-1.5
-K2 = 7.56;    % bar^-1
+R  = 8.31447;               % gaskon.
+P  = 1;                     % bar 
+k  = @(T) A*exp(-Ea./(R.*T)); % mol/kg cat.*s*bar vid 550 C
+K1 = 22.9;                  % bar^-1.5
+K2 = 7.56;                  % bar^-1
 
 % Hämtar molflöden och temperatur
-FA = U(1); FB = U(2); 
-FC = U(3); FD = U(4); 
+FA = U(1); 
+FB = U(2); 
+FC = U(3); 
+FD = U(4); 
 T = U(5);
 F_tot = FA + FB + FC + FD;
 
 % Hämtar Cp
-Cp_A = Cp(1); Cp_B = Cp(2); 
-Cp_C = Cp(3); Cp_D = Cp(4);
+Cp_A = Cp(1); 
+Cp_B = Cp(2); 
+Cp_C = Cp(3); 
+Cp_D = Cp(4);
 
 %Beräknar partialtryck
-PA = P*(FA/F_tot); PB = P*(FB/F_tot);
-PC = P*(FA/F_tot); PD = P*(FD/F_tot);
+PA = P*(FA/F_tot); 
+PB = P*(FB/F_tot);
+PC = P*(FA/F_tot); 
+PD = P*(FD/F_tot);
+
+% Arrenius
 
 % Ke som funktion av T
-Ke = @(T)(2.1*10^7) * exp(-122000/(R*T)) % bar
-
+Ke = @(T)(2.1*10^7) * exp(-122000/(R*T)); % bar
 
 % Beräknar rA
-rA = (k* (PA - (PB*PC/Ke(T)))) / (1 + K1*PB*sqrt(PC) + sqrt(K2*PC));
+rA = (k(T).* (PA - (PB*PC/Ke(T)))) ./ (1 + (K1*PB*sqrt(PC)) + (sqrt(K2*PC)));
 
 sumFCp = FA*Cp_A + FB*Cp_B + FC*Cp_C + FD*Cp_D;
 
@@ -42,17 +49,18 @@ dFC =  rA;
 dFD =  0;
 
 % deltaH för A -> B + C
-dH_A = Cp_A(T0-T);
-dH_B = Cp_B(T0-T);
-dH_C = Cp_C(T0-T);
-dH   = dH_B+dH_C-dH_A;
+dH  = dH0 + (Cp_B+Cp_C-Cp_B); 
 
 % deltaH för en viss temp ska beräknas via Cp vid en viss temp här för att
 % få en dTdV
 
 
 % Temperaturbalans
-dTdV = r * -dH / (sumFCp)
+dTdV = rA * -dH / (sumFCp);
 
-dUdV = [dFA dFB dFC dFD dTdV];
+dUdV = [dFA 
+        dFB 
+        dFC 
+        dFD 
+        dTdV];
 end
--- Reaktor/Reaktor.m
+++ Reaktor/Reaktor.m
@@ -17,43 +17,49 @@ k = 0.0596; % mol/kg cat.*s*bar vid 550 C
 K1 = 22.9; % bar^-1
 K2 = 7.56; % bar^-1
 
+T_reaktorer = [298 750 500 750 500];
+
+CPcoeff_H2O = [72.43 1.039*10^-2 -1.497*10^-6 0 ];
+CPcoeff_H2 = [27.14 0.009274 -1.38*10^-5 7.645*10^-9];
+CPcoeff_ISOBUTAN = [-1.39 0.3847 -1.846*10^-4 2.895*10^-8];
+CPcoeff_ISOBUTEN = [16.05 0.2804 -1.091*10^-4 9.098*10^-9];
+
+% Arrenius ekv.
+A = k/exp(-Ea/(823*R));
+
 % REAKTOR 1
 
 % Cp beräkning
 T = T_reaktorer(2); %K, equlibrium-conversion mot T börjar avta vid denna ~temp
-% H2O
-A_H2O = 30.092;
-B_H2O = 6.832514;
-C_H2O = 6.793435;
-D_H2O = -2.53448;
-E_H2O = 0.082139;
-Cp_H2O = A_H2O + (B_H2O.*(T.*10^-3)) + (C_H2O.*((T.*10^-3).^2)) + (D_H2O.*((T.*10^-3).^3))+(E_H2O./((T.*10^-3)^2));%[kJ/mol]
 
-% H2
-A_H2 = 33.066178;
-B_H2 = -11.363417;
-C_H2 = 11.432816;
-D_H2 = 2.772874;
-E_H2 = -0.158558;
-Cp_H2 = A_H2 + (B_H2.*(T.*10^-3)) + (C_H2.*((T.*10^-3).^2)) + (D_H2.*((T.*10^-3).^3))+(E_H2./((T.*10^-3)^2));%[kJ/mol]
+CPcoeff = CPcoeff_H2O;
+CpH2O = Cp_calc(T,CPcoeff);
+
+CPcoeff = CPcoeff_H2;
+CpH2 = Cp_calc(T,CPcoeff);
+
+CPcoeff = CPcoeff_ISOBUTAN;
+CpButan = Cp_calc(T,CPcoeff);
+
+CPcoeff = CPcoeff_ISOBUTEN;
+CpButen = Cp_calc(T,CPcoeff);
+
+Cp = [CpButan CpButen CpH2 CpH2O];
+
+dH0_Butan   = -17.9*10^3;
+dH0_Buten   = -134.2*10^3;
+dH0_H2      = 0;
+
+dH0         = dH0_Buten+dH0_H2-dH0_Butan;
+
+U0          = [128 5 0 1091 750];
+
+Vstart = 0; %Volym m3
+Vfinal = 10; 
+Vspan = [Vstart Vfinal];
+[V,U] = ode15s(@PFR_ode,[Vstart Vfinal],U0,[],Cp,dH0,A,Ea);
 
 % REAKTOR 2
 
 % Cp beräkning
 T = T_reaktorer(4); %K, equlibrium-conversion mot T börjar avta vid denna ~temp
-%H2O
-A_H2O = 30.092;
-B_H2O = 6.832514;
-C_H2O = 6.793435;
-D_H2O = -2.53448;
-Cp_H2O = (A_H2O + (B_H2O.*(T.*10^-3)) + (C_H2O.*((T.*10^-3).^2)) + (D_H2O.*((T.*10^-3).^3)))%[kJ/mol]
-D_H2O = -2.53448;
-Cp_H2O = (A_H2O + (B_H2O.*(T.*10^-3)) + (C_H2O.*((T.*10^-3).^2)) + (D_H2O.*((T.*10^-3).^3)));%[kJ/mol]
-
-% H2
-A_H2 = 33.066178;
-B_H2 = -11.363417;
-C_H2 = 11.432816;
-D_H2 = 2.772874;
-Cp_H2 = (A_H2 + (B_H2.*(T.*10^-3)) + (C_H2.*((T.*10^-3).^2)) + (D_H2.*((T.*10^-3).^3)));%[kJ/mol]
-

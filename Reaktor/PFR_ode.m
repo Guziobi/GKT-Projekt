a907292329ebb3eeb5a811c2f2f017916e8d1158
ronge11 <johan.ronge010327@gmail.com>
2022-04-08 11:08:25 +0200

    Fixade dH0 så att det blir positivt och inte negativt

--- Reaktor/PFR_ode.m
+++ Reaktor/PFR_ode.m
@@ -1,4 +1,4 @@
-function dUdV = PFR_ode(W, U, dH0,k,K1,K2,P)
+function dUdV = PFR_ode(W,U,dH0,k,K1,K2,P,A,Ea)
 
 % A = isobutAn (C4H10)
 % B = isobutEn (C4H8)
@@ -13,31 +13,26 @@ FD = U(4);
 T = U(5);
 F_tot = FA + FB + FC + FD;
 
-<<<<<<< HEAD
+
 CPcoeff_H2O = [72.43 1.039*10^-2 -1.497*10^-6 0];
 CPcoeff_H2 = [27.14 0.009274 -1.38*10^-5 7.645*10^-9];
 CPcoeff_ISOBUTAN = [-1.39 0.3847 -1.846*10^-4 2.895*10^-8];
 CPcoeff_ISOBUTEN = [16.05 0.2804 -1.091*10^-4 9.098*10^-9];
-=======
+
 % Data
 R  = 8.31447;               % gaskon.
 P  = 1;                     % bar 
 k  = A*exp(-Ea./(R.*T));    % mol/kg cat.*s*bar vid 550 C
 K1 = 22.9;                  % bar^-1.5
 K2 = 7.56;                  % bar^-1
->>>>>>> 96bd975e9251b4d2352214d5dba87c9bb60fd6f5
 
-CPcoeff = CPcoeff_ISOBUTAN;
-CpA = @(T) Cp_calc(T,CPcoeff);
+CpA = @(T) Cp_calc(T,CPcoeff_ISOBUTAN);
 
-CPcoeff = CPcoeff_ISOBUTEN;
-CpB = @(T) Cp_calc(T,CPcoeff);
+CpB = @(T) Cp_calc(T,CPcoeff_ISOBUTEN);
 
-CPcoeff = CPcoeff_H2;
-CpC = @(T) Cp_calc(T,CPcoeff);
+CpC = @(T) Cp_calc(T,CPcoeff_H2);
 
-CPcoeff = CPcoeff_H2O;
-CpD = @(T) Cp_calc(T,CPcoeff);
+CpD = @(T) Cp_calc(T,CPcoeff_H2O);
 
 %Beräknar partialtryck
 PA = P*(FA/F_tot); 
@@ -46,11 +41,11 @@ PC = P*(FC/F_tot);
 PD = P*(FD/F_tot);
 
 % Ke som funktion av T
-<<<<<<< HEAD
+
 Ke = (2.1*10^7) * exp(-122*10^3/(8.314*T)); % bar
-=======
+
 Ke = (2.1*10^7) * exp(-122000/(R*T)); % bar
->>>>>>> 96bd975e9251b4d2352214d5dba87c9bb60fd6f5
+
 
 % Beräknar rA
 r = k*(PA - ((PB*PC)/Ke))/(1+K1*PB*((PC)^0.5)+(K2*PC)^0.5);
--- Reaktor/Reaktor.m
+++ Reaktor/Reaktor.m
@@ -9,19 +9,15 @@ clc, clear
 
 % Data
 R = 8.31447;
-<<<<<<< HEAD
 P = 1;              % [bar]
 rho_cat = 1120;     % [kg/m3]
-W = 7000;              %[kg]
+W = 7000;           %[kg]
 Ea = 141e3;         %[J/mol]
 k = 0.0596;         % mol/kg cat.*s*bar vid 550 C
 K1 = 22.9;          % bar^-1
 K2 = 7.56;          % bar^-1
 
 % REAKTOR 1
-dH0_Butan   = -134.2*10^3;
-dH0_Buten   = -17.9*10^3;
-=======
 rho_cat = 1120; % [kg/m3]
 V = 10; %[m3]
 Tin_reak = 750;
@@ -30,7 +26,7 @@ k = 0.0596; % mol/kg cat.*s*bar vid 550 C
 K1 = 22.9; % bar^-1
 K2 = 7.56; % bar^-1
 
-CPcoeff_H2O = [72.43 1.039*10^-2 -1.497*10^-6 0 ];
+CPcoeff_H2O = [72.43 1.039*10^-2 -1.497*10^-6 0 ];          %Koffecienter för Cp [A B C D]
 CPcoeff_H2 = [27.14 0.009274 -1.38*10^-5 7.645*10^-9];
 CPcoeff_ISOBUTAN = [-1.39 0.3847 -1.846*10^-4 2.895*10^-8];
 CPcoeff_ISOBUTEN = [16.05 0.2804 -1.091*10^-4 9.098*10^-9];
@@ -50,20 +46,19 @@ CpButen = Cp_calc(T,CPcoeff_ISOBUTEN);
 
 Cp = [CpButan CpButen CpH2 CpH2O];
 
-dH0_Butan   = -17.9*10^3;
-dH0_Buten   = -134.2*10^3;
->>>>>>> 96bd975e9251b4d2352214d5dba87c9bb60fd6f5
+dH0_Butan   = -134.2*10^3; %Formationsentalpi Butan
+dH0_Buten   = -17.9*10^3;  %Formationsentalpi Buten
 dH0_H2      = 0;
 
-dH0 = dH0_Buten+dH0_H2-dH0_Butan;
+dH0 = dH0_Buten+dH0_H2-dH0_Butan; %Formationsentalpi för rektionen
 
-<<<<<<< HEAD
+
 U01 = [128/3.6 5/3.6 0 1091/3.6 750];
 
 Wstart1 = 0; %Volym m3
 Wfinal1 = W; 
 Wspan1 = [Wstart1 Wfinal1];
-[W1,U1] = ode15s(@PFR_ode,Wspan1,U01,[],dH0,k,K1,K2,P);
+[W1,U1] = ode15s(@PFR_ode,Wspan1,U01,[],dH0,k,K1,K2,P,A,Ea);
 
 X1 = (U1(1,1)-U1(:,1))/U1(1,1);
 T1 = U1(:,5);
@@ -75,10 +70,10 @@ title('Reaktor 1'), xlabel('W [kg]'), ylabel('Omsättningsgrad, X')
 figure (2);
 plot(T1,X1);
 title('Reaktor 1'), xlabel('T [K]'), ylabel('Omsättningsgrad, X')
-=======
+
             %Butan Buten H2 H2O Temp
 U0          = [128 5 0 1091 750];
->>>>>>> 96bd975e9251b4d2352214d5dba87c9bb60fd6f5
+
 
 
 % REAKTOR 2
@@ -86,7 +81,7 @@ U02 = [U1(1,1) U1(1,2) U1(1,3) U1(1,4) 750];
 Wstart2 = 0; %Volym m3
 Wfinal2 = 6000; 
 Wspan2 = [Wstart2 Wfinal2];
-[W2,U2] = ode15s(@PFR_ode,Wspan2,U02,[],dH0,k,K1,K2,P);
+[W2,U2] = ode15s(@PFR_ode,Wspan2,U02,[],dH0,k,K1,K2,P,A,Ea);
 
 X2 = (U2(1,1)-U2(:,1))/U2(1,1);
 T2 = U2(:,5);
@@ -111,11 +106,10 @@ figure(6);
 plot(X,T)
 title('Reaktor 1 och 2'),xlabel('Omsättningsgrad, X'),ylabel('T [K]')
 
-<<<<<<< HEAD
-=======
+
 % Cp beräkning
-T = Tin_reak; %K, equlibrium-conversion mot T börjar avta vid denna ~temp
+%T = Tin_reak; %K, equlibrium-conversion mot T börjar avta vid denna ~temp
 
 
-plot(V, U(:,1))
->>>>>>> 96bd975e9251b4d2352214d5dba87c9bb60fd6f5
+%plot(V, U(:,1))
+

dbd79564ee6663304cf62d295cdfc67378187d88
ronge11 <johan.ronge010327@gmail.com>
2022-04-09 23:44:04 +0200

    Lekte runt lite med T och W cat. för att maximera X, samt ändra en plot från W mot T till V mot T

--- Reaktor/Reaktor.m
+++ Reaktor/Reaktor.m
@@ -10,10 +10,10 @@ clc, clear
 % Data
 R = 8.31447;
 P = 1;              % [bar]
-T = 750;
+T = 800;            % [K]
 rho_cat = 1120;     % [kg/m3]
-W = 7000;              %[kg]
-Ea = 141e3;         %[J/mol]
+W = 5000;           % [kg]
+Ea = 141e3;         % [J/mol]
 k = 0.0596;         % mol/kg cat.*s*bar vid 550 C
 K1 = 22.9;          % bar^-1
 K2 = 7.56;          % bar^-1
@@ -27,11 +27,11 @@ Cp = [CPcoeffA; CPcoeffB; CPcoeffC; CPcoeffD];
 
 dH0A   = -134.2e3;       % [kJ mol^-1]
 dH0B   = -17.9e3;        % [kJ mol^-1]
-dH0C      = 0;           % [kJ mol^-1]
-dHr0 = dH0B+dH0C-dH0A;   % [kJ mol^-1]
+dH0C   = 0;              % [kJ mol^-1]
+dHr0   = dH0B+dH0C-dH0A; % [kJ mol^-1]
 
 % REAKTOR 1
-U01 = [128/3.6 5/3.6 0 1091/3.6 750];
+U01 = [140/3.6 6/3.6 0 1190/3.6 800]; %[mol s^-1]
 
 Wstart1 = 0; %Volym m3
 Wfinal1 = W; 
@@ -41,39 +41,47 @@ Wspan1 = [Wstart1 Wfinal1];
 X1 = (U1(1,1)-U1(:,1))/U1(1,1);
 T1 = U1(:,5);
 
+V1= W1./rho_cat;
+
 figure(1);
-plot(W1,X1)
-title('Reaktor 1'), xlabel('W [kg]'), ylabel('Omsättningsgrad, X')
+plot(V1,X1)
+title('Reaktor 1'), xlabel('V [m3]'), ylabel('Omsättningsgrad, X')
 
 figure (2);
 plot(T1,X1);
 title('Reaktor 1'), xlabel('T [K]'), ylabel('Omsättningsgrad, X')
 
+
 % REAKTOR 2
-U02 = [U1(1,1) U1(1,2) U1(1,3) U1(1,4) 750];
+U02 = [U1(1,1) U1(1,2) U1(1,3) U1(1,4) 800];
 Wstart2 = 0; %Volym m3
-Wfinal2 = 6000; 
+Wfinal2 = 5000; 
 Wspan2 = [Wstart2 Wfinal2];
 [W2,U2] = ode15s(@PFR_ode,Wspan2,U02,[],dHr0,k,K1,K2,P,Cp);
 
 X2 = (U2(1,1)-U2(:,1))/U2(1,1);
 T2 = U2(:,5);
 
+V2= W2./rho_cat;
+
 figure(3);
-plot(W2,X2)
-title('Reaktor 2'),xlabel('W [kg]'), ylabel('Omsättningsgrad, X')
+plot(V2,X2)
+title('Reaktor 2'),xlabel('V [m3]'), ylabel('Omsättningsgrad, X')
 
 figure (4);
 plot(T2,X2);
 title('Reaktor 2'), xlabel('T [K]'), ylabel('Omsättningsgrad, X')
 
+% KOSTNADSBERÄKNING REAKTOR 2
+ReaktorV2 = Wfinal2./rho_cat; %[m3]
+
 % BÅDA REAKTORERNA
-W = [W1; [W2+W1(end)]]; 
+V = [V1; [V2+V1(end)]]; 
 X = [X1; [X2+X1(end)]]; 
 T = [T1; T2]; 
 figure(5);
-plot(W,X)
-title('Reaktor 1 och 2'),xlabel('W [kg]'), ylabel('Omsättningsgrad, X')
+plot(V,X)
+title('Reaktor 1 och 2'),xlabel('V [m3]'), ylabel('Omsättningsgrad, X')
 
 figure(6);
 plot(X,T)

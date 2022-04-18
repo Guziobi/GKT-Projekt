e37d4ce66b180256d3424dfcd9e5b4f05ab47c9d
ronge11 <johan.ronge010327@gmail.com>
2022-04-18 18:13:29 +0200

    La till enheter och kommenterade lite av koden

--- Reaktor/PFR_ode.m
+++ Reaktor/PFR_ode.m
@@ -1,6 +1,6 @@
 function dUdV = PFR_ode(W, U, dHr0,k,K1,K2,P,Cp)
 
-R  = 8.31447;               % gaskon.
+R = 8.31447;               % gaskon.
 
 % A = isobutAn (C4H10)
 % B = isobutEn (C4H8)
@@ -16,22 +16,22 @@ T = U(5);
 F_tot = FA + FB + FC + FD;
 
 % Cp-funktioner beroende av T
-CpA = @(T) Cp_calc(T,Cp(1,:));
-CpB = @(T) Cp_calc(T,Cp(2,:));
-CpC = @(T) Cp_calc(T,Cp(3,:));
-CpD = @(T) Cp_calc(T,Cp(4,:));
+CpA = @(T) Cp_calc(T,Cp(1,:));  %Cp för ämne A (isobutan) vid T
+CpB = @(T) Cp_calc(T,Cp(2,:));  %Cp för ämne B (isobuten) vid T
+CpC = @(T) Cp_calc(T,Cp(3,:));  %Cp för ämne C (H2) vid T
+CpD = @(T) Cp_calc(T,Cp(4,:));  %Cp för ämne D (H2O) vid T
 
 %Beräknar partialtryck
-PA = P*(FA/F_tot); 
-PB = P*(FB/F_tot);
-PC = P*(FC/F_tot); 
-PD = P*(FD/F_tot);
+PA = P*(FA/F_tot);  %Partialtryck för ämne A (isobutan) för inflöde till reaktor
+PB = P*(FB/F_tot);  %Partialtryck för ämne B (isobuten)) för inflöde till reaktor
+PC = P*(FC/F_tot);  %Partialtryck för ämne C (H2)) för inflöde till reaktor
+PD = P*(FD/F_tot);  %Partialtryck för ämne D (H2O) för inflöde till reaktor
 
 % Ke som funktion av T
-Ke = (2.1*10^7) * exp(-122*10^3/(8.314*T)); % bar
+Ke = (2.1*10^7) * exp(-122*10^3/(8.314*T)); % [bar]
 
 % Beräknar r
-r = k*(PA - ((PB*PC)/Ke))/(1+K1*PB*((PC)^0.5)+(K2*PC)^0.5);
+r = k*(PA - ((PB*PC)/Ke))/(1+K1*PB*((PC)^0.5)+(K2*PC)^0.5); % [mol kg cat.^-1 s^-1 bar^-0.5]
 
 % Massbalanser för A -> B + C
 dFA = -r;

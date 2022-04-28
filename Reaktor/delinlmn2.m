56ec87b9cae3f2cdfc79ef334ca3366a5bd287bd
ronge11 <johan.ronge010327@gmail.com>
2022-04-28 15:27:17 +0200

    la till en plot för en ensam reaktor och räkna klart pris för inköp

--- Reaktor/delinlmn2.m
+++ Reaktor/delinlmn2.m
@@ -41,6 +41,19 @@ Wspan1 = [Wstart1 Wfinal1];
 T1 = U1(:,5); %[K]
 V1= W1./rho_cat; %[m3]
 
+% REAKTOR 1 alternativ
+U0_alt1 = [131/3.6 0/3.6 0 1310/3.6 T_reaktor1]; %[mol s^-1]
+
+Wstart_alt1 = 0; %Massa cat. [kg]
+Wfinal_alt1 = 35000; 
+Wspan_alt1 = [Wstart_alt1 Wfinal_alt1];
+[W_alt1,U_alt1] = ode15s(@PBR_ode,Wspan_alt1,U0_alt1,[],dHr0,k,K1,K2,P,Cp);
+
+T_alt = U_alt1(:,5); %[K]
+V_alt= W_alt1./rho_cat; %[m3]
+
+X_alt1= (U_alt1(1,1)-U_alt1(:,1))/U_alt1(1,1);
+
 % REAKTOR 2
 U02 = [U1(end,1) U1(end,2) U1(end,3) U1(end,4) T_reaktor2];
 Wstart2 = 0; %Massa cat. [kg]
@@ -67,6 +80,8 @@ FC = U(:,3);
 % Plottar omsättningsgraden mot massan katalysator för de båda reaktorerna
 figure(1);
 plot(W,X)
+hold on
+plot(W_alt1,X_alt1,'r--')
 title('Reaktor 1 och 2'),xlabel('Katalysatormassa, W [kg]'), ylabel('Omsättningsgrad, X')
 hold on
 plot([Wfinal1 Wfinal1],[0 1],'k--')     % Avgränsning mellan reaktor 1 och 2
@@ -81,7 +96,7 @@ figure(3);
 plot(W,FA,W,FB)
 hold on
 plot([Wfinal1 Wfinal1],[0 40],'k--')   % Avgränsning mellan reaktor 1 och 2
-title('Molflöde för specier vs Katalysatormassa')
+title('Molflöde för specier vs katalysatormassa')
 xlabel('Katalysatormassa, W [kg]'), ylabel('Molflöde [mol/s]')
 legend('isobutan','isobuten och vätgas')
 
@@ -97,7 +112,7 @@ rad1 = (Vol1/(4*pi)).^(1/3); % Radie på reaktor 1 [m]
 D1 = 2*rad1; % Diameter på reaktor 1 [m]
 Smax = 74.5*10^6; % Maximalt tillåtna spänningen över 900F [N/m2]
 P_konc = P*10^5*1.1; % Konstruktionstryck, 10% mer än arbetstryck [Pa]
-wall1= (P_konc*D1)./((2*Smax*1)-(1.2*P_konc)); %[m]
+wall1= ((P_konc*D1)./((2*Smax*1)-(1.2*P_konc))).*10^3; %[mm]
 if D1<=2
     if wall1<7
         wall1=7;
@@ -111,7 +126,7 @@ end
 Mantel1 = 8*pi*(rad1^2);
 V_wall1 = Mantel1*(wall1*10^-3); %[m3]
 rho_wall = 8000; %[kg/m3]
-mass_wall1 = V_wall1./rho_wall; %[kg]
+mass_wall1 = V_wall1.*rho_wall; %[kg]
 % Horisontell reaktor
 a = 12800;
 b = 73;
@@ -121,16 +136,52 @@ cost_reak1_cat = cost_reak1*1.5; % Kostnad för reaktor 1 + katalysator år 2010
 cost1_2020 = (cost_reak1_cat*(569/532.9))*9.99*4; % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
 disp(['Kostnad för reaktor 1 år 2020 (SEK):   ',num2str(cost1_2020)]), disp(' ')
 
-%% REAKTOR 2
+% REKTOR 1 alt.
+T_Falt = (9/5)*(T_reaktor1-273.15)+32; % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
+Vol_alt = Wfinal_alt1./rho_cat;
+rad_alt = (Vol_alt/(4*pi)).^(1/3); % Radie på reaktor 1 [m]
+D_alt = 2*rad_alt; % Diameter på reaktor 1 [m]
+Smax = 74.5*10^6; % Maximalt tillåtna spänningen över 900F [N/m2]
+P_konc = P*10^5*1.1; % Konstruktionstryck, 10% mer än arbetstryck [Pa]
+wall_alt= ((P_konc*D_alt)./((2*Smax*1)-(1.2*P_konc))).*10^3; %[mm]
+if D_alt<=2
+    if wall_alt<7
+        wall_alt=7;
+    end
+end
+if D_alt>2 && D_alt<=2.5
+    if wall_alt<9
+        wall_alt=9;
+    end
+end
+if D_alt>2.5 && D_alt<=3
+     if wall_alt<10
+        wall_alt=10;
+    end
+end
+Mantel_alt = 8*pi*(rad_alt^2);
+V_wall_alt = Mantel_alt*(wall_alt*10^-3); %[m3]
+rho_wall = 8000; %[kg/m3]
+mass_wall_alt = V_wall_alt.*rho_wall; %[kg]
+% Horisontell reaktor
+a = 12800;
+b = 73;
+n = 0.85;
+cost_reak_alt = a+(b*(mass_wall_alt^n)); % Kostnad för reaktor 1 år 2010 (CEPCI = 532.9)
+cost_reak_alt_cat = cost_reak_alt*1.5; % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
+cost_alt_2020 = (cost_reak_alt_cat*(569/532.9))*9.99*4; % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
+disp(['Kostnad för alternativ reaktor 1 år 2020 (SEK):   ',num2str(cost_alt_2020)]), disp(' ')
+
+% REAKTOR 2
 T_F2 = (9/5)*(T_reaktor2-273.15)+32; % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
 Vol2 = Wfinal2./rho_cat;
 rad2 = (Vol2/(4*pi)).^(1/3); % Radie på reaktor 1 [m]
 D2 = 2*rad2; % Diameter på reaktor 1 [m]
 Smax = 74.5*10^6; % Maximalt tillåtna spänningen över 900F [N/m2]
 P_konc = P*10^5*1.1; % Konstruktionstryck, 10% mer än arbetstryck [Pa]
-wall2= (P_konc*D2)./((2*Smax*1)-(1.2*P_konc)); %[m]
+wall2= ((P_konc*D2)./((2*Smax*1)-(1.2*P_konc))).*10^3; %[mm]
 if D2<=2
-    if wall2<7
+    if (wall2)<7
         wall2=7;
     end
 end
@@ -140,9 +191,9 @@ if D2>2 && D2<=2.5
     end
 end
 Mantel2 = 8*pi*(rad2^2);
-V_wall2 = Mantel2*(wall2*10^-3); %[m3]
+V_wall2 = Mantel2*(wall2.*10^-3); %[m3]
 rho_wall = 8000; %[kg/m3]
-mass_wall2 = V_wall2./rho_wall; %[kg]
+mass_wall2 = V_wall2.*rho_wall; %[kg]
 % Horisontell reaktor
 a = 12800;
 b = 73;
@@ -151,6 +202,8 @@ cost_reak2 = a+(b*(mass_wall2^n)); % Kostnad för reaktor 1 år 2010 (CEPCI = 53
 cost_reak2_cat = cost_reak2*1.5; % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
 cost2_2020 = (cost_reak2_cat*(569/532.9))*9.99*4; % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
 disp(['Kostnad för reaktor 2 år 2020 (SEK):   ',num2str(cost2_2020)]), disp(' ')
+cost_allareakt = cost1_2020 + cost2_2020;
+disp(['Kostnad för reaktor 1 och 2 år 2020 (SEK):   ',num2str(cost_allareakt)]), disp(' ')
 %%
 % Plottar 
 

a00db9327d5163b1951c84b35ce08cca56856b84
mullerse <muller.sebbe@gmail.com>
2022-04-28 16:03:54 +0200

    Script för kylning 5-6 troligtvis klart

--- Värmeväxling/ekonomi.m
+++ Värmeväxling/ekonomi.m
@@ -1,11 +1,11 @@
-function res = ekonomi(NTU, Cmin, Cmax, Cmm, Ka, U, Thin, Tcin, tdrift, beta)
+function res = ekonomi(NTU, Cmm, Ka, U, Tcin, Thin, tdrift, beta)
 
 bosse = Ka./(U.*(Thin - Tcin).*tdrift.*beta);
 
 %Derivatan av epsilon map NTU
 dedNTU=(exp(-NTU+Cmm.*NTU)-2.*exp(-NTU+Cmm.*NTU).*Cmm+Cmm.^2.*exp(-NTU+Cmm.*NTU))./(1-Cmm.*exp(-NTU+Cmm.*NTU)).^2;
 
-%fsolve ska lösa ekvationen de/dNTU - bosse = 0
+%fsolve ska lösa ekvationen de/dNTU = bosse eller de/dNTU - bosse = 0
 res = dedNTU - bosse;
 
 end

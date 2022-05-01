b13fa4d85e740b6f86c8eeec967048aabca98aa5
axeltho <axel.thoren01@gmail.com>
2022-05-01 17:34:33 +0200

    Fixade beräkningen av väggtjockleken, men får för låga värden dessvärre. La in funktioner från separationsbiten. Förkortade lite upprepande kod.

--- delinlmn3.m
+++ delinlmn3.m
@@ -7,6 +7,8 @@ clc, clear, close all
 % vätgas   = C
 % vatten   = D
 
+%% REAKTOR
+
 % Data
 R = 8.31447;        % Gaskonstanten [J mol^-1 K^-1]
 P = 1;              % [bar]
@@ -41,6 +43,7 @@ Wspan1 = [Wstart1 Wfinal1];
 
 T1 = U1(:,5); %[K]
 V1= W1./rho_cat; %[m3]
+X1 = (U1(1,1)-U1(end,1))./U1(1,1);
 
 % REAKTOR 2
 U02 = [U1(end,1) U1(end,2) U1(end,3) U1(end,4) T_reaktor2];
@@ -51,6 +54,7 @@ Wspan2 = [Wstart2 Wfinal2];
 
 T2 = U2(:,5); %[K]
 V2 = W2./rho_cat; %[m3]
+X2 = (U2(1,1)-U2(end,1))./U2(1,1);
 
 % REAKTOR 1 alternativ
 Wstart_alt1 = 0; %Massa cat. [kg]
@@ -75,7 +79,7 @@ FA = U(:,1);
 FB = U(:,2);
 FC = U(:,3); 
 
-xf = U(end,2)/(U(end,2)+U(end,1)); % Komposition in till destillationskolonnen
+xf = U(end,2)/(U(end,1)+U(end,2)); % Komposition in till destillationskolonnen
 
 % PLOTTAR:
 % Plottar omsättningsgraden mot massan katalysator för de båda reaktorerna
@@ -104,99 +108,96 @@ legend('isobutan','isobuten och vätgas','Location','northwest')
 
 %% Tryckkätlsväggens (reaktorväggens) tjocklek
 % REKTOR 1
-T_F1 = (9/5)*(T_reaktor1-273.15)+32; % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
+T_F1 = (9/5)*(T_reaktor1-273.15)+32;	                  % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
 Vol1 = Wfinal1./rho_cat;
-rad1 = (Vol1/(4*pi)).^(1/3); % Radie på reaktor 1 [m]
-D1 = 2*rad1; % Diameter på reaktor 1 [m]
-Smax = 74.5*10^6; % Maximalt tillåtna spänningen över 900F [N/m2]
-P_konc = P*10^5*1.1; % Konstruktionstryck, 10% mer än arbetstryck [Pa]
-wall1= ((P_konc*D1)./((2*Smax*1)-(1.2*P_konc))).*10^3; %[mm]
-if D1<=2
-    if wall1<7
-        wall1=7;
-    end
-end
-if D1>2 && D1<=2.5
-    if wall1<9
-        wall1=9;
-    end
-end
+D1 = (2*Vol1/pi)^(1/3);                                   % Diameter på reaktor 1 [m]
+rad1 = D1/2;    
+Smax = 74.5E6;                                            % Maximalt tillåtna spänningen över 900F [N/m2]
+P_konstr = P*10^5*1.1;                                    % Konstruktionstryck, 10% mer än arbetstryck [Pa]
+E = 1;                                                    % Svetsverkningsgrad
+wall1= ((P_konstr*D1)/((2*Smax*E)-(1.2*P_konstr))).*10^3; %[mm]
+rho_wall = [7900 8000];  
+
+% if D1<=2
+%     if wall1<7
+%         wall1=7;
+%     end
+% end
+% if D1>2 && D1<=2.5
+%     if wall1<9
+%         wall1=9;
+%     end
+% end
+
 Mantel1 = 8*pi*(rad1^2);
-V_wall1 = Mantel1*(wall1*10^-3); %[m3]
-rho_wall = 8000; %[kg/m3]
-mass_wall1 = V_wall1.*rho_wall; %[kg]
+V_wall1 = Mantel1*(wall1*10^-3);        %[m3]
+mass_wall1 = V_wall1(1)*rho_wall(1);    %[kg]
+
 % Horisontell reaktor pris
-a = 12800;
-b = 73;
-n = 0.85;
-cost_reak1 = a+(b*(mass_wall1^n)); % Kostnad för reaktor 1 år 2010 (CEPCI = 532.9)
-cost_reak1_cat = cost_reak1*1.5; % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
-cost1_2020 = (cost_reak1_cat*(569/532.9))*9.99*4; % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
+Reaktor_param = [12800 73 0.85];
+
+cost_reak1 = Cost(mass_wall1,Reaktor_param);        % Kostnad för reaktor 1 år 2010 (CEPCI = 532.9)
+cost_reak1_cat = cost_reak1*1.5;                    % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
+cost1_2020 = (cost_reak1_cat*(569/532.9))*9.99*4;   % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
 
 % REKTOR 1 alt.
-T_Falt = (9/5)*(T_reaktor1-273.15)+32; % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
+T_Falt = (9/5)*(T_reaktor1-273.15)+32;                           % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
 Vol_alt = Wfinal_alt1./rho_cat;
-rad_alt = (Vol_alt/(4*pi)).^(1/3); % Radie på reaktor 1 [m]
-D_alt = 2*rad_alt; % Diameter på reaktor 1 [m]
-Smax = 74.5*10^6; % Maximalt tillåtna spänningen över 900F [N/m2]
-P_konc = P*10^5*1.1; % Konstruktionstryck, 10% mer än arbetstryck [Pa]
-wall_alt= ((P_konc*D_alt)./((2*Smax*1)-(1.2*P_konc))).*10^3; %[mm]
-if D_alt<=2
-    if wall_alt<7
-        wall_alt=7;
-    end
-end
-if D_alt>2 && D_alt<=2.5
-    if wall_alt<9
-        wall_alt=9;
-    end
-end
-if D_alt>2.5 && D_alt<=3
-     if wall_alt<10
-        wall_alt=10;
-    end
-end
+rad_alt = (Vol_alt/(4*pi)).^(1/3);                               % Radie på reaktor 1 [m]
+D_alt = 2*rad_alt;                                               % Diameter på reaktor 1 [m]
+wall_alt= ((P_konstr*D_alt)./((2*Smax*1)-(1.2*P_konstr))).*10^3; %[mm]
+
+% if D_alt<=2
+%     if wall_alt<7
+%         wall_alt=7;
+%     end
+% end
+% if D_alt>2 && D_alt<=2.5
+%     if wall_alt<9
+%         wall_alt=9;
+%     end
+% end
+% if D_alt>2.5 && D_alt<=3
+%      if wall_alt<10
+%         wall_alt=10;
+%     end
+% end
+
 Mantel_alt = 8*pi*(rad_alt^2);
-V_wall_alt = Mantel_alt*(wall_alt*10^-3); %[m3]
-rho_wall = 8000; %[kg/m3]
-mass_wall_alt = V_wall_alt.*rho_wall; %[kg]
+V_wall_alt = Mantel_alt*(wall_alt*10^-3);   %[m3]
+mass_wall_alt = V_wall_alt.*rho_wall(1);    %[kg]
+
 % Horisontell reaktor pris
-a = 12800;
-b = 73;
-n = 0.85;
-cost_reak_alt = a+(b*(mass_wall_alt^n)); % Kostnad för reaktor 1 år 2010 (CEPCI = 532.9)
-cost_reak_alt_cat = cost_reak_alt*1.5; % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
-cost_alt_2020 = (cost_reak_alt_cat*(569/532.9))*9.99*4; % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
+cost_reak_alt = Cost(mass_wall_alt,Reaktor_param);
+cost_reak_alt_cat = cost_reak_alt*1.5;                       % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
+cost_alt_2020 = (cost_reak_alt_cat*(569/532.9))*9.99*4;      % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
 
 % REAKTOR 2
-T_F2 = (9/5)*(T_reaktor2-273.15)+32; % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
+T_F2 = (9/5)*(T_reaktor2-273.15)+32;                        % Temperatur i Farenheit, om över 900F måste rostfritt stål användas, max 1500F
 Vol2 = Wfinal2./rho_cat;
-rad2 = (Vol2/(4*pi)).^(1/3); % Radie på reaktor 1 [m]
-D2 = 2*rad2; % Diameter på reaktor 1 [m]
-Smax = 74.5*10^6; % Maximalt tillåtna spänningen över 900F [N/m2]
-P_konc = P*10^5*1.1; % Konstruktionstryck, 10% mer än arbetstryck [Pa]
-wall2= ((P_konc*D2)./((2*Smax*1)-(1.2*P_konc))).*10^3; %[mm]
-if D2<=2
-    if (wall2)<7
-        wall2=7;
-    end
-end
-if D2>2 && D2<=2.5
-    if wall2<9
-        wall2=9;
-    end
-end
+rad2 = (Vol2/(4*pi)).^(1/3);                                % Radie på reaktor 1 [m]
+D2 = 2*rad2;                                                % Diameter på reaktor 1 [m]
+wall2= ((P_konstr*D2)./((2*Smax*1)-(1.2*P_konstr))).*10^3;  % [mm]
+
+% if D2<=2
+%     if (wall2)<7
+%         wall2=7;
+%     end
+% end
+% if D2>2 && D2<=2.5
+%     if wall2<9
+%         wall2=9;
+%     end
+% end
+
 Mantel2 = 8*pi*(rad2^2);
-V_wall2 = Mantel2*(wall2.*10^-3); %[m3]
-rho_wall = 8000; %[kg/m3]
-mass_wall2 = V_wall2.*rho_wall; %[kg]
+V_wall2 = Mantel2*(wall2.*10^-3);   %[m3]
+mass_wall2 = V_wall2.*rho_wall(1);  %[kg]
+
 % Horisontell reaktor
-a = 12800;
-b = 73;
-n = 0.85;
-cost_reak2 = a+(b*(mass_wall2^n)); % Kostnad för reaktor 1 år 2010 (CEPCI = 532.9)
-cost_reak2_cat = cost_reak2*1.5; % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
-cost2_2020 = (cost_reak2_cat*(569/532.9))*9.99*4; % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
+cost_reak2 = Cost(mass_wall2,Reaktor_param);
+cost_reak2_cat = cost_reak2*1.5;                         % Kostnad för reaktor 1 + katalysator år 2010 (CEPCI = 532.9)
+cost2_2020 = (cost_reak2_cat*(569/532.9))*9.99*4;        % Kostnad för reaktor 1 + katalysator samt montering år 2020 i SEK
 cost_allareakt = cost1_2020 + cost2_2020;
 
 %% Utskrivning av resultat
@@ -209,6 +210,7 @@ disp(['Vätgas (mol/s):                                   ',num2str(U1(end,3))])
 disp(['Temperatur (K):                                   ',num2str(U1(end,5))])
 disp(['Katalysatormassa (kg):                            ',num2str(Wfinal1)])
 disp(['Volym (m^3):                                      ',num2str(Vol1)])
+disp(['Omsättningsgrad:                                  ',num2str(X1)])
 disp([' '])
 
 % Ut ur reaktor 2
@@ -219,6 +221,11 @@ disp(['Vätgas (mol/s):                                   ',num2str(U(end,3))])
 disp(['Temperatur (K):                                   ',num2str(U(end,5))])
 disp(['Katalysatormassa (kg):                            ',num2str(Wfinal2)])
 disp(['Volym (m^3):                                      ',num2str(Vol2)])
+disp(['Omsättningsgrad:                                  ',num2str(X2)])
+disp([' '])
+disp(['Omsättningsgrad TOTAL:                            ',num2str(X(end))])
+disp(['Katalysatormassa (kg) TOTAL:                      ',num2str(Wfinal1+Wfinal2)])
+disp(['Volym (m^3) TOTAL:                                ',num2str(Vol1+Vol2)])
 disp(['xf:                                               ',num2str(xf)])
 disp([' '])
 
@@ -233,6 +240,7 @@ disp(['Kostnad för reaktor 1 och 2 år 2020 (SEK):        ',num2str(cost_allare
 
 %% Funktioner
 
+% Reaktorfunktioner
 function dUdV = PBR_ode(W, U, dHr0,A,E,K1,K2,P,Cp)
 
 R = 8.31447;               % gaskon.
@@ -298,4 +306,46 @@ function Cp = Cp_calc(T, CPcoeff)
 
     Cp = A + B*T + C*T.^2 + D*T.^3;
 
+end
+
+% Separationsfunktioner
+function [gamma1, gamma2] = wilson(x1,W12,W21)
+    x2 = 1 - x1;
+
+    gamma1 = exp(-log(x1+W12.*x2) + x2.*(W12./(x1+W12.*x2) - W21./(W21.*x1+x2)));
+    gamma2= exp(-log(x2+W21.*x1) - x1.*(W12./(x1+W12.*x2) - W21./(W21.*x1+x2)));
+
+end
+
+function P_sat = antoine(T,Ant)
+
+    A = Ant(1);
+    B = Ant(2); 
+    C = Ant(3);
+    P_sat = exp(A-(B./(T+C))); 
+
+end
+
+function res = find_Tb(T,x1,gamma1,gamma2,Ant1,Ant2,P)
+
+A1 = Ant1(1); B1 = Ant1(2); C1 = Ant1(3);
+A2 = Ant2(1); B2 = Ant2(2); C2 = Ant2(3);
+
+P1 = exp(A1-(B1./(T+C1)));
+P2 = exp(A2-(B2./(T+C2)));
+
+y1 = (x1.*gamma1.*P1)./P;
+y2 = ((1-x1).*gamma2.*P2)./P;
+
+res=y1+y2-1;
+
+end
+
+% Kostnadsfunktioner
+function kostnad = Cost(S,Param)
+    a = Param(1);
+    b = Param(2);
+    n = Param(3);
+    
+    kostnad = a + b*S.^n;
 end
